c09b24daee0e33cc73a2e48370579a122fe1943b
JuiceLoc <alejo120821@gmail.com>
2026-07-30 12:51:07 -0500

    Shake de la camara
    Ahora cuando el jugador pierde puntos la camara da feedback

--- Assets/Scripts/CameraFall.cs
+++ Assets/Scripts/CameraFall.cs
@@ -10,8 +10,15 @@ public class CameraFall : MonoBehaviour
     [Header("Frecuencia de recálculo")]
     [SerializeField] private float updateInterval = 3f;
 
+    [Header("Camera Shake")]
+    [SerializeField] private float shakeDuration = 0.2f;
+    [SerializeField] private float shakeStrength = 0.25f;
+
     private float currentFallSpeed;
     private float timer;
+
+    private float shakeTimeRemaining;
+    private Vector3 currentShakeOffset;
 
     private void Start()
     {
@@ -20,6 +27,10 @@ public class CameraFall : MonoBehaviour
 
     private void Update()
     {
+        // Esto hace que la camara no se desvie
+        transform.position -= currentShakeOffset;
+        currentShakeOffset = Vector3.zero;
+
         timer += Time.deltaTime;
 
         if (timer >= updateInterval)
@@ -28,12 +39,44 @@ public class CameraFall : MonoBehaviour
             RecalculateSpeed();
         }
 
-        transform.position += Vector3.down * currentFallSpeed * Time.deltaTime;
+        // Movimiento normal hacia abajo
+        transform.position +=
+            Vector3.down *
+            currentFallSpeed *
+            Time.deltaTime;
+
+        // Aplica el shake
+        if (shakeTimeRemaining > 0f)
+        {
+            shakeTimeRemaining -= Time.deltaTime;
+
+            Vector2 randomOffset =
+                Random.insideUnitCircle * shakeStrength;
+
+            currentShakeOffset = new Vector3(
+                randomOffset.x,
+                randomOffset.y,
+                0f
+            );
+
+            transform.position += currentShakeOffset;
+        }
     }
 
     private void RecalculateSpeed()
     {
-        float targetSpeed = baseFallSpeed + (Leak.TotalPoints * speedPerPoint);
-        currentFallSpeed = Mathf.Min(targetSpeed, maxFallSpeed);
+        float targetSpeed =
+            baseFallSpeed +
+            (Leak.TotalPoints * speedPerPoint);
+
+        currentFallSpeed = Mathf.Min(
+            targetSpeed,
+            maxFallSpeed
+        );
+    }
+
+    public void Shake()
+    {
+        shakeTimeRemaining = shakeDuration;
     }
 }
