f83f12734bbc4fda42c595b381418f9c67e0a66a
Katherinne <laura.rojaspardo@gmail.com>
2026-07-30 15:05:02 -0500

    Score

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -12,6 +12,6 @@ public class ScoreUI : MonoBehaviour
 
     private void Update()
     {
-        scoreText.text = "Puntos: " + Leak.TotalPoints;
+        scoreText.text = Leak.TotalPoints + " pts";
     }
 }
